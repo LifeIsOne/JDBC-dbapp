895a866da6365e9dfa47201dabf8c803298c6682
LifeIsOne <didtmdgh258@gmail.com>
2024-01-19 17:17:44 +0900

    JDBC API 실행

--- src/main/java/BankApp.java
+++ src/main/java/BankApp.java
@@ -2,7 +2,6 @@ import db.DBConnection;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
 public class BankApp {
     public static void main(String[] args) {
@@ -10,11 +9,12 @@ public class BankApp {
 
         // 얘가 buffer
         try {
-            PreparedStatement pstmt = conn.prepareStatement("inset ino account_tb(password, balance, created_at) values(?, ?, now())");
-            // 0이 없이 1부터 시작
+            PreparedStatement pstmt = conn.prepareStatement("insert into account_tb(password, balance, created_at) values(?, ?, now())");
+            // 0 없이 1부터 시작
             pstmt.setString(1,"1234");
             pstmt.setInt(2,1000);
-            pstmt.executeUpdate();
+            int num = pstmt.executeUpdate();
+            System.out.println(num);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
