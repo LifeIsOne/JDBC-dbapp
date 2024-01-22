ead93834f1c04b07ac2c7c8813adae5dfa10bf1a
LifeIsOne <didtmdgh258@gmail.com>
2024-01-22 17:41:53 +0900

    BankApp 컨트롤러 만들기

--- src/main/java/BankApp.java
+++ src/main/java/BankApp.java
@@ -1,22 +1,66 @@
 import dao.BankDAO;
+import model.Account;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class BankApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("삭제할 계좌번호를 입력해주세요 : ");
-        int number = sc.nextInt();
+        //  http://bank.com/account GET
+        //  http://bank.com/account/10 GET
+        //  http://bank.com/account POST
+        //  http://bank.com/account/1 DELETE
+        //  http://bank.com/account/1 PUT
 
-        BankDAO dao = new BankDAO();
+        //  /account
+        //  /account/1
+        //  /account
+        //  /account/1
+        //  /account/1
+        System.out.println("HTTP 메서드를 입력하세요");
+        String method = sc.nextLine();
 
-        int result = dao.deleteByNumber(number);
+        System.out.println("식별자를 입력하세요.");
+        String action = sc.nextLine();
 
-        if (result == 1)
-            System.out.println("삭제되었습니다.");
-        else
-            System.out.println("삭제하지 못했습니다.");
+        String body = "";
+
+        BankDAO bankDAO = new BankDAO();
+        if (method.equals("GET")){                  //  SELECT
+            if (action.equals("/account")) {
+                List<Account> accountList = bankDAO.selectAll();
+                System.out.println(accountList);
+            } else if (action.equals("/account/1")) {
+                Account account = bankDAO.selectByNumber(1);
+                System.out.println(account);
+            }
+        } else if (method.equals("POST")) {         //  INSERT
+            System.out.println("Enter body data");
+            body = sc.nextLine();
+
+            //  password=1234&balance=1000 이런식으로 코딩할거임
+            String[] st1 = body.split("&");
+            String password = st1[0].split("=")[1];
+            int balance = Integer.parseInt(st1[1].split("=")[1]);
+
+            if (action.equals("/account")){
+                bankDAO.insert(password,balance);
+            }
+        } else if (method.equals("PUT")) {          //  UPDATE
+            System.out.println("Enter body data");
+            body = sc.nextLine();
+
+
+
+            String[] st1 = body.split("&");
+            String password = st1[0].split("=")[1];
+
+        } else if (method.equals("DELETE")) {       //  DELETE
+            int number = Integer.parseInt(sc.nextLine());
+            bankDAO.deleteByNumber(number);
+        }
 
     }
 }
