1fa515f5afe288326e7aed4bfb918c565a7a0914
LifeIsOne <didtmdgh258@gmail.com>
2024-01-22 11:07:21 +0900

    bankapp ã실행

---
+++ src/main/java/dao/BankDAO.java
@@ -0,0 +1,74 @@
+package dao;
+
+import db.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+/**
+ *  DAO - Data Access Object
+ *  SRP - 단일책임 원칙
+ */
+
+
+public class BankDAO {
+
+    public int deleteByNumber(int number) {
+        Connection conn = DBConnection.getInstance();               // 소켓
+        try {
+            String sql =
+                    "delete from account_tb where number =?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);   // 오류나면 catch로 감
+
+            pstmt.setInt(1, number);                 // DB의 리턴
+            int num = pstmt.executeUpdate();
+
+            return num;
+        } catch (Exception e) {
+            e.printStackTrace();                                    // 오류나면 피범벅
+        }
+        return -1;
+    }
+
+    public int insert(String password, int balance){
+        Connection conn = DBConnection.getInstance();
+        try {
+            String sql =
+                    "insert into account_tb(password, balance, created_at) values(?, ?, now())";    // number는 auto_increment
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, password);
+            pstmt.setInt(2, balance);
+
+            int num = pstmt.executeUpdate();
+
+            return num;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    public int updateByNumber(int balance, int number){
+        Connection conn = DBConnection.getInstance();
+        try {
+            String sql =
+                    "update account_tb set balance = ? where number = ?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, balance);
+            pstmt.setInt(2, number);
+
+            int num = pstmt.executeUpdate();
+
+            return num;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+    public void selectByNumber(){
+
+    }
+    public void  selectAll(){
+
+    }
+}
